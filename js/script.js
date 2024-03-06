00c30fa008a34dd708edacdecd0595e4d31f5ef7
Franco Ditale <francocrack000@gmail.com>
2024-03-06 10:27:49 -0300

    Problemas de codigo y console log en consola

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let ingresoParaLogeados = false;
-
 function buscarUsuario(username) {
     return cuentasRegistradas.find(user => user.usuarioNuevo === username)
 }
@@ -26,35 +24,44 @@ class RegistroCuenta {
 
 /* REGISTRO DE USUARIO */
 
-alert("Registro de cuenta");
-let continuar = true;
+const formulario = document.getElementById("registrationForm");
 
-while (continuar) {
-    let registrarUsuario = prompt("Elija Usuario");
-    let registrarEmail = prompt("Elija Email");
-    let registrarContraseña = prompt("Elija Contraseña");
-    let registrarContraseñaConfirmar = prompt("Confirmar Contraseña");
+formulario.addEventListener("submit", createAccountAndRedirect);
 
-    if (registrarContraseña === registrarContraseñaConfirmar) {
-        if (registrarUsuario && registrarEmail) {
-            const usuario = new RegistroCuenta(registrarUsuario, registrarEmail, registrarContraseña, registrarContraseñaConfirmar);
+const crearCuentaBtn = document.getElementById("btn-registrar");
 
-            agregarRegistros(cuentasRegistradas, usuario)
+crearCuentaBtn.addEventListener("click", function() {
+    createAccountAndRedirect(); 
+});
 
-            alert("Has sido registrado correctamente");
-            continuar = false;
-        } else {
-            alert("Por favor, ingresa al menos un nombre de usuario o correo electrónico.");
+function createAccount() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password === confirmPassword) {
+        if (username && email) {
+            const usuario = new RegistroCuenta(username, email, password, confirmPassword);
+            agregarRegistros(cuentasRegistradas, usuario);
+            console.log(cuentasRegistradas); 
+            return true; 
+            alert("Por favor, ingresa al menos un nombre de usuario y un correo electrónico.");
+            return false;
         }
     } else {
-        alert("Estimado Usuario, Las contraseñas no coinciden")
+        alert("Las contraseñas no coinciden");
+        return false;
     }
 }
 
-console.log(cuentasRegistradas);
+function createAccountAndRedirect() {
+    if (createAccount()) { 
+        window.location.href = "./index.html"; 
+    }
+}
 
-
-/* INICIO SESIÓN */
+/* INICIO SESIÓN
 
 alert("Inicio De Sesión")
 
@@ -84,7 +91,8 @@ iniciarSesion();
 
 
 
-/* CAMBIO DE CONTRASEÑA  */
+CAMBIO DE CONTRASEÑA 
+
 alert("Cambio de contraseña");
 
 function cambiarContraseña() {
@@ -128,7 +136,7 @@ function cambiarContraseña() {
     }
 }
 
-cambiarContraseña();
+cambiarContraseña(); */
 
 
 
