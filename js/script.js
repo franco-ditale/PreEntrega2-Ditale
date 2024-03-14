0884b104680e03039fc4f57457389c7764826818
Franco Ditale <francocrack000@gmail.com>
2024-03-14 15:42:50 -0300

    Arreglo de codigo + LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 function buscarUsuario(username) {
-    return cuentasRegistradas.find(user => user.usuarioNuevo === username);
+    return cuentasRegistradas.find(user => user.usuarioNuevo === username)
 }
 
 function buscarContraseña(user, password) {
     return user.contraseñaNueva === password;
 }
 
-function agregarRegistros(cuentasRegistradas, usuario) {
-    return cuentasRegistradas.push(usuario);
-}
+const cuentasRegistradas = JSON.parse(localStorage.getItem('database')) || [];
 
-const cuentasRegistradas = [];
+
+function agregarRegistros(usuario) {
+    cuentasRegistradas.push(usuario);
+    
+    localStorage.setItem("database", JSON.stringify(cuentasRegistradas))
+    
+}
 
 class RegistroCuenta {
     constructor(usuarioNuevo, email, contraseñaNueva, contraseñaConfirmar) {
@@ -25,20 +29,16 @@ class RegistroCuenta {
 
 document.addEventListener("DOMContentLoaded", function () {
     const formulario = document.getElementById("registrationForm");
-    if (formulario) { // Verificar si el formulario existe en el DOM
+    if (formulario) { 
         formulario.addEventListener("submit", crearCuentaAndRedireccion);
-    } else {
-        console.error("No se encontró el formulario de registro en el DOM.");
-    }
+    } 
 
     const crearCuentaBtn = document.getElementById("btn-registrar");
-    if (crearCuentaBtn) { // Verificar si el botón de registro existe en el DOM
+    if (crearCuentaBtn) { 
         crearCuentaBtn.addEventListener("click", function (event) {
-            crearCuentaAndRedireccion(event); // Pasar el evento como parámetro
+            crearCuentaAndRedireccion(event); 
         });
-    } else {
-        console.error("No se encontró el botón de registro en el DOM.");
-    }
+    } 
 });
 
 function crearCuenta() {
@@ -50,8 +50,8 @@ function crearCuenta() {
     if (password === confirmPassword) {
         if (username && email) {
             const usuario = new RegistroCuenta(username, email, password, confirmPassword);
-            agregarRegistros(cuentasRegistradas, usuario);
-            console.log(cuentasRegistradas);
+            agregarRegistros(usuario); 
+            console.log("Nuevo usuario agregado:", usuario); 
             return true;
         } else {
             alert("Por favor, ingresa al menos un nombre de usuario y un correo electrónico.");
@@ -62,7 +62,7 @@ function crearCuenta() {
 }
 
 function crearCuentaAndRedireccion(event) {
-    event.preventDefault(); // Detener el envío del formulario por defecto
+    event.preventDefault(); 
     if (crearCuenta()) {
         alert("Redireccionando");
         location.href = "./iniciosesion.html";
@@ -73,24 +73,6 @@ function crearCuentaAndRedireccion(event) {
 
 /* INICIO SESIÓN */
 
-function iniciarSesionYRedireccionar() {
-    const username = document.getElementById("username-is").value;
-    const password = document.getElementById("password-is").value;
-
-    if (username && password) {
-        const usuarioEncontrado = buscarUsuario(username);
-
-        if (usuarioEncontrado && buscarContraseña(usuarioEncontrado, password)) {
-            alert("¡Bienvenido " + username + "!");
-            window.location.href = "./main.html"; // Redireccionar a otra sección HTML
-        } else {
-            alert("Datos incorrectos. Por favor, inténtalo nuevamente.");
-        }
-    } else {
-        alert("Por favor, ingresa tu nombre de usuario y contraseña.");
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function () {
     const formularioLogin = document.getElementById("formularioInicioSesion");
     if (formularioLogin) {
@@ -98,21 +80,40 @@ document.addEventListener("DOMContentLoaded", function () {
             event.preventDefault();
             iniciarSesionYRedireccionar();
         });
-    } else {
-        console.error("No se encontró el formulario de inicio de sesión en el DOM.");
-    }
+    } 
 
     const iniciarSesionBtn = document.getElementById("btn-iniciarSesion");
     if (iniciarSesionBtn) {
         iniciarSesionBtn.addEventListener("click", function() {
             iniciarSesionYRedireccionar();
         });
-    } else {
-        console.error("No se encontró el botón de inicio de sesión en el DOM.");
-    }
+    } 
 });
 
+function iniciarSesionYRedireccionar() {
+    
+    const username = document.getElementById("username-is").value;
+    const password = document.getElementById("password-is").value;
 
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log(cuentasRegistradas);
+
+    if (username && password) {
+        const usuarioEncontrado = buscarUsuario(username);
+
+        console.log("Usuario encontrado:", usuarioEncontrado);
+
+        if (usuarioEncontrado && buscarContraseña(usuarioEncontrado, password)) {
+            alert("¡Bienvenido " + username + "!");
+            window.location.href = "./main.html"; 
+        } else {
+            alert("Datos incorrectos. Por favor, inténtalo nuevamente.");
+        }
+    } else {
+        alert("Por favor, ingresa tu nombre de usuario y contraseña.");
+    }
+}
 
 /* CAMBIAR CONTRASEÑA */
 
@@ -145,66 +146,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.getElementById("btn-cambiar").addEventListener("click", cambiarContraseña); */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* CAMBIO DE CONTRASEÑA 
-
-alert("Cambio de contraseña");
-
-function cambiarContraseña() {
-
-    let resetContraseña = false;
-
-    while (!resetContraseña) {
-        const usuarioParaReset = document.getElementById("username-cambiar").value;
-        const emailUsuario = document.getElementById("email-cambiar").value;
-
-        let usuarioEncontrado = buscarUsuario(usuarioParaReset);
-
-        if (usuarioEncontrado && usuarioEncontrado.email === emailUsuario) {
-
-            let cambioDeContraseña = prompt("Ingresar Nueva Contraseña");
-            let cambioDeContraseñaConfirmar = prompt("Confirmar Contraseña");
-
-            if (cambioDeContraseña === cambioDeContraseñaConfirmar) {
-                usuarioEncontrado.contraseñaNueva = cambioDeContraseña;
-                alert("Has restablecido tu contraseña correctamente");
-                resetContraseña = true;
-
-                let ingresarUsuario = prompt("Ingresar Usuario");
-                let ingresarContraseña = prompt("Ingresar Contraseña");
-
-                let encontrarUsuario = buscarUsuario(ingresarUsuario);
-
-                if (encontrarUsuario && buscarContraseña(encontrarUsuario, ingresarContraseña)) {
-                    alert("Bienvenido " + ingresarUsuario)
-        
-                } else {
-                    alert("Datos Incorrectos");
-                }
-
-            } else {
-                alert("Las contraseñas no coinciden");
-            }
-        } else {
-            alert("Los datos no existen");
-        }
-    }
-}
-
-cambiarContraseña(); */
 
 
 
